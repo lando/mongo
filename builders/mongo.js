58c5522c91e41d4e95dc7240cbcc3b10f23ede41
Alec Reynolds <alec+git@lando.dev>
2023-11-02 10:53:26 -0700

    Run official Mongo images for versions above 5.

--- builders/mongo.js
+++ builders/mongo.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const { log } = require('console');
 // Modules
 const _ = require('lodash');
 const path = require('path');
+const semver = require('semver');
 
 // Builder
 module.exports = {
@@ -12,8 +14,8 @@ module.exports = {
     supported: ['7.0', '6.0', '5.0', '4.4', '4.2', '4.1', '4.0', '3.6'],
     legacy: ['4.1'],
     pinPairs: {
-      '7.0': 'bitnami/mongodb:7.0.2-debian-11-r7',
-      '6.0': 'bitnami/mongodb:6.0.11-debian-11-r1',
+      '7.0': 'mongo:7.0.2-jammy',
+      '6.0': 'mongo:6.0.11-jammy',
       '5.0': 'bitnami/mongodb:5.0.3-debian-10-r8',
       '4.4': 'bitnami/mongodb:4.4.9-debian-10-r10',
       '4.2': 'bitnami/mongodb:4.2.6-debian-10-r33',
@@ -23,7 +25,6 @@ module.exports = {
     },
     patchesSupported: true,
     confSrc: path.resolve(__dirname, '..', 'config'),
-    healthcheck: ['mongo', 'tests', '--eval', 'db.runCommand("ping").ok'],
     port: '27017',
     remoteFiles: {
       database: '/bitnami/mongodb/conf/lando.conf',
@@ -33,7 +34,19 @@ module.exports = {
   builder: (parent, config) => class LandoMongoDb extends parent {
     constructor(id, options = {}) {
       options = _.merge({}, config, options);
-      const mongo = {
+      const mongoOfficial = {
+        image: `mongo:${options.version}`,
+        command: 'docker-entrypoint.sh mongod',
+        environment: {
+          ALLOW_EMPTY_PASSWORD: 'yes',
+          LANDO_NEEDS_EXEC: 'DOEEET',
+        },
+        volumes: [
+          `${options.data}:/data/db`,
+        ],
+      };
+
+      const mongoBitnami = {
         image: `bitnami/mongodb:${options.version}`,
         command: '/launch.sh',
         environment: {
@@ -46,6 +59,17 @@ module.exports = {
           `${options.data}:/bitnami`,
         ],
       };
+      
+      // If options.version is greater than 5.0, use the official mongo image
+      const mongoAbove5 = semver.valid(semver.coerce(options.version)) ?
+        semver.gte(semver.coerce(options.version), '6.0.0')
+        : options._app.log.error(`Invalid version ${options.version} for mongo service`);
+
+      const mongo = mongoAbove5 ? mongoOfficial : mongoBitnami;
+      if (!options.healthcheck) options.healthcheck = mongoAbove5 ?
+        ['mongosh', 'tests', '--eval', 'db.runCommand("ping").ok']
+        : ['mongo', 'tests', '--eval', 'db.runCommand("ping").ok']; 
+      
       // Send it downstream
       super(id, options, {services: _.set({}, options.name, mongo)});
     };
