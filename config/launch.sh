bd3e5f670f5e31f918d57659b3081cb94d4b1623
Mike Pirog <mike@lando.dev>
2025-09-01 11:41:45 -0400

    3.7 seems to be experimental or something so roll back to latest 3.6

--- config/launch.sh
+++ config/launch.sh
@@ -6,6 +6,8 @@ set -e
 # Detect and run the correct entrypoint script. THANKS BITNAMI!
 if [ -f "/opt/bitnami/scripts/mongodb/entrypoint.sh" ]; then
   /opt/bitnami/scripts/mongodb/entrypoint.sh /opt/bitnami/scripts/mongodb/run.sh
+elif [ -f "/app-entrypoint.sh" ]; then
+  /app-entrypoint.sh /run.sh
 else
   /entrypoint.sh /run.sh
 fi
